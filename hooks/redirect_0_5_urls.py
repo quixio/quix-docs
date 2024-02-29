f69f8bf1ac160c12509cc859d476ea1bfff80159
tbedford <tony@quix.ai>
2024-02-29 11:56:43 +0000

    [fix] - redirects

--- hooks/redirect_0_5_urls.py
+++ hooks/redirect_0_5_urls.py
@@ -8,8 +8,8 @@ def on_files(files: Files, config):
     Redirect URLs "quix-streams/v2-0-latest/***" to "quix-streams/***"
     using "redirects" plugin
     """
-    new_prefix = "quix-streams/v2-0-latest/"
-    old_prefix = "quix-streams/"
+    new_prefix = "quix-streams/"
+    old_prefix = "quix-streams/v2-0-latest/"
 
     redirect_plugin = config.get("plugins", {}).get("redirects")
     redirects = redirect_plugin.config.get("redirect_maps", {})
